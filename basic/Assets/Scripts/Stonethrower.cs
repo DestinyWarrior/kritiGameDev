c5926c902a20ee754042933cebfbf00b8e73c65f
mynknight <mayankchoudhary651@gmail.com>
2023-02-15 05:31:45 +0530

    Stone Thrower added
    Added a layer and mc is in that layer to avoid collision with itself.

---
+++ basic/Assets/Scripts/Stonethrower.cs
@@ -0,0 +1,47 @@
+//If You Like Our Content, 
+//Than Hit The Subscribe Button :)
+
+using System.Collections;
+using UnityEngine;
+
+public class Stonethrower : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private SpringJoint2D springJoint;
+    private bool isPressed;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        springJoint = GetComponent<SpringJoint2D>();
+    }
+
+    void Update()
+    {
+        if (isPressed)
+        {
+            rb.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        isPressed = true;
+        rb.isKinematic = true;
+    }
+    private void OnMouseUp()
+    {
+        isPressed = false;
+        rb.isKinematic = false;
+        StartCoroutine(Release());
+    }
+
+    IEnumerator Release()
+    {
+        yield return new WaitForSeconds(0.15f);
+        GetComponent<SpringJoint2D>().enabled = false;
+    }
+}
+
+
+
