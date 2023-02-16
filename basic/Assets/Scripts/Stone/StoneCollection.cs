caf5f7265ebe317bbc9c7c862ee7a6dc250c1a78
mynknight <mayankchoudhary651@gmail.com>
2023-02-16 17:07:42 +0530

    traps, death, platforms
    tags added, points

---
+++ basic/Assets/Scripts/Stone/StoneCollection.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StoneCollection : MonoBehaviour
+{
+    public int stone_count = 0;
+    [SerializeField] private Text stoneCount;
+     
+   
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("points"))
+        {
+            Destroy(collision.gameObject);
+            stone_count++;
+            stoneCount.text = "Stone:" + stone_count;
+
+        }
+    }
+}
